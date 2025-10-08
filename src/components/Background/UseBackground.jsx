f9dca27add941f678a9f58567f27273e3d7c081f
hyemin <ohyemin1113@gmail.com>
2025-10-09 04:55:25 +0900

    feat : UseBackgroung 컴포넌트 구현

---
+++ src/components/Background/UseBackground.jsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+const UseBackground = () => {
+  return <Container></Container>;
+};
+
+export default UseBackground;
+
+const Container = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -1;
+  width: 100%;
+  height: 100%;
+  background-color: #0f50a0;
+`;
