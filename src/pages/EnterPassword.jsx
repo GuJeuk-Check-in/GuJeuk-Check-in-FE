a1651615b61e32c57544c63da95327f2f2b0ecbd
hyemin <ohyemin1113@gmail.com>
2025-10-09 04:53:51 +0900

    feat : EnterPassword 페이지 구현

---
+++ src/pages/EnterPassword.jsx
@@ -0,0 +1,111 @@
+import PasswordBackground from '../components/Background/PasswordBackground';
+import LeftPage from '../components/Form/LeftPage';
+import RightPage from '../components/Form/RightPage';
+import LabeledInput from '../components/Form/LabeledInput';
+import styled from '@emotion/styled';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const EnterPassword = () => {
+  const [currentPW, setCurrentPW] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const navigate = useNavigate();
+
+  const CORRECT_PASSWORD = '1234';
+
+  const handleConfirm = () => {
+    if (currentPW.trim() === '') {
+      setErrorMessage('비밀번호를 입력해주세요.');
+      return;
+    }
+
+    if (currentPW === CORRECT_PASSWORD) {
+      setErrorMessage('');
+      navigate('/user-visit-list');
+    } else {
+      setErrorMessage('일치하지 않습니다.');
+    }
+  };
+  return (
+    <>
+      <PasswordBackground />
+      <MainWrapper>
+        <LeftPage />
+        <RightPage
+          title="관리자 비밀번호 입력"
+          buttonContent="확인"
+          onClick={handleConfirm}
+          buttonBottom="200px"
+        >
+          <EmptyInputSpace />
+          <InputAndErrorWrapper>
+            <LabeledInput
+              label=""
+              placeholder="비밀번호를 입력해주세요."
+              type="password"
+              value={currentPW}
+              onChange={(e) => {
+                setCurrentPW(e.target.value);
+                setErrorMessage('');
+              }}
+              isError={!!errorMessage}
+            />
+            {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
+          </InputAndErrorWrapper>
+          <UpdatePasswordButtonFixer>
+            <UpdatePasswordButton onClick={() => navigate('/update-password')}>
+              비밀번호 변경하기
+            </UpdatePasswordButton>
+          </UpdatePasswordButtonFixer>
+        </RightPage>
+      </MainWrapper>
+    </>
+  );
+};
+
+export default EnterPassword;
+
+const MainWrapper = styled.div`
+  min-height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ErrorMessage = styled.p`
+  color: #ff5a5a;
+  font-size: 20px;
+  width: 100%;
+  text-align: right;
+  padding-right: 100px;
+`;
+
+const EmptyInputSpace = styled.div`
+  height: 80px;
+`;
+
+const InputAndErrorWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const UpdatePasswordButton = styled.button`
+  border: none;
+  background: none;
+  color: #a4dfff;
+  font-size: 18px;
+  cursor: pointer;
+`;
+
+const UpdatePasswordButtonFixer = styled.div`
+  position: absolute;
+  bottom: 130px;
+  left: 82%;
+  transform: translateX(-50%);
+  width: calc(100% - 80px);
+  text-align: left;
+`;
