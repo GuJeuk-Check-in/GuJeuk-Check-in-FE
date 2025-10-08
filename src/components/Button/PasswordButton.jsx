ac2973d0d752586c0d65f9458ac00ae9cd21bac2
hyemin <ohyemin1113@gmail.com>
2025-10-09 04:55:56 +0900

    feat : PasswordButton 컴포넌트 구현

---
+++ src/components/Button/PasswordButton.jsx
@@ -0,0 +1,35 @@
+import styled from '@emotion/styled';
+
+const PasswordButton = ({ content, onClick }) => {
+  return (
+    <Container onClick={onClick}>
+      <Content>{content}</Content>
+    </Container>
+  );
+};
+
+export default PasswordButton;
+
+const Container = styled.div`
+  width: 172px;
+  height: 67px;
+  border-radius: 50px;
+  border: 2px solid #ffffff;
+  background-color: #3d72b3;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  transition: 0.3s;
+
+  &:hover {
+    background-color: #c2d5ec;
+  }
+`;
+
+const Content = styled.span`
+  color: #ffffff;
+  font-size: 24px;
+  margin: 0;
+`;
