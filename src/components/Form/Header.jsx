f72049cef2d945f6be650d418d0d8f0f19bc4267
hyemin <ohyemin1113@gmail.com>
2025-10-16 21:27:44 +0900

    feat : Header 컴포넌트에 주요 메뉴 네비게이션 버튼 추가

--- src/components/Form/Header.jsx
+++ src/components/Form/Header.jsx
@@ -1,15 +1,34 @@
+import { useNavigate } from 'react-router-dom';
 import HeaderButton from '../Button/HeaderButton';
 import Logo from '../../assets/Logo.png';
 import styled from '@emotion/styled';
 
 const Header = ({ title }) => {
+  const navigate = useNavigate();
+
   return (
     <Container>
-      <img src={Logo} alt="로고 이미지" />
-      <HeaderButton>시설 이용 목록 조회</HeaderButton>
-      <HeaderButton>시설 이용 기록 추가</HeaderButton>
-      <HeaderButton>방문 목적 커스텀</HeaderButton>
-      <HeaderButton>회원 목록 조회</HeaderButton>
+      {' '}
+      <img
+        src={Logo}
+        alt="로고 이미지"
+        onClick={() => navigate('/user-visit-list')}
+      />{' '}
+      <HeaderButton onClick={() => navigate('/user-visit-list')}>
+        시설 이용 목록 조회
+      </HeaderButton>
+      <Diver />
+      <HeaderButton onClick={() => navigate('/user-detail')}>
+        시설 이용 기록 추가
+      </HeaderButton>
+      <Diver />
+      <HeaderButton onClick={() => navigate('/purpose-custom')}>
+        방문 목적 커스텀
+      </HeaderButton>
+      <Diver />
+      <HeaderButton onClick={() => navigate('/user-information')}>
+        회원 목록 조회
+      </HeaderButton>
       <Title>{title}</Title>
     </Container>
   );
@@ -21,10 +40,14 @@ const Container = styled.div`
   background-color: #ffffff;
   display: flex;
   align-items: center;
-
   gap: 20px;
   width: 100%;
   height: 12.04vh;
+  box-sizing: border-box;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1000;
 
   img {
     width: 256px;
@@ -37,7 +60,14 @@ const Container = styled.div`
 
 const Title = styled.h1`
   font-size: 32px;
+  font-weight: 600;
   color: #393939;
   margin-left: auto;
   margin-right: 20px;
+`;
+
+const Diver = styled.div`
+  width: 1.5px;
+  height: 20px;
+  background-color: #aaa;
 `;
