5d5791d813c3d1daa175a07b1a5a466df628ce68
hyemin <ohyemin1113@gmail.com>
2025-10-16 21:33:30 +0900

    feat : VisitForm을 통합하여 시설 이용 등록 페이지 레이아웃 완성

--- src/pages/UserDetail.jsx
+++ src/pages/UserDetail.jsx
@@ -2,25 +2,46 @@ import Header from '../components/Form/Header';
 import VisitForm from '../components/Form/VisitForm';
 import UseBackground from '../components/Background/UseBackground';
 import styled from '@emotion/styled';
+import useVisitStore from '../store/useVisitStore';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 const UserDetail = () => {
+  const addVisit = useVisitStore((state) => state.addVisit);
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({
+    name: '',
+    male: '',
+    female: '',
+    date: '',
+  });
+
+  const handleSubmit = () => {
+    addVisit(form);
+    navigate('/user-visit-list');
+  };
   return (
-    <>
+    <Container>
       <UseBackground />
-      <Header title="시설 이용 기록" />
+      <Header title="시설 이용 기록 추가" />
       <ContentWrapper>
-        <VisitForm />
+        <VisitForm form={form} setForm={setForm} onSubmit={handleSubmit} />
       </ContentWrapper>
-    </>
+    </Container>
   );
 };
 
 export default UserDetail;
+
+const Container = styled.div`
+  padding-top: 12.04vh;
+`;
 
 const ContentWrapper = styled.div`
   width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 40px 0;
+  min-height: 100vh;
 `;
