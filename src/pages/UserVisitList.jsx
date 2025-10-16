9c108f82819fd92ea243a0355a78f06defbfa3dc
hyemin <ohyemin1113@gmail.com>
2025-10-16 21:32:27 +0900

    refactor : 방문 목록 페이지의 하드코딩 데이터 제거 및 Zustand 상태 연동

--- src/pages/UserVisitList.jsx
+++ src/pages/UserVisitList.jsx
@@ -1,38 +1,43 @@
 import UseBackground from '../components/Background/UseBackground';
 import Header from '../components/Form/Header';
 import UserVisitCard from '../components/Form/UserVisitCard';
+import { useNavigate } from 'react-router-dom';
+import useVisitStore from '../store/useVisitStore';
+import styled from '@emotion/styled';
 
 const UserVisitList = () => {
+  const visits = useVisitStore((state) => state.visits);
+  const removeVisit = useVisitStore((state) => state.removeVisit);
+  const navigate = useNavigate();
+
+  const handleDelete = (id, name) => {
+    if (window.confirm(`${name}님의 기록을 삭제하시겠습니까?`)) {
+      removeVisit(id);
+      alert(`${name}님의 기록이 삭제되었습니다.`);
+    }
+  };
+
   return (
-    <>
+    <Container>
       <UseBackground />
-      <Header title="시설 이용 목록 조회"></Header>
-      <UserVisitCard
-        name="오혜민"
-        male="1"
-        female="2"
-        date="2025년 11월 13일"
-      />
-      <UserVisitCard
-        name="오혜민"
-        male="1"
-        female="2"
-        date="2025년 11월 13일"
-      />
-      <UserVisitCard
-        name="오혜민"
-        male="1"
-        female="2"
-        date="2025년 11월 13일"
-      />
-      <UserVisitCard
-        name="오혜민"
-        male="1"
-        female="2"
-        date="2025년 11월 13일"
-      />
-    </>
+      <Header title="시설 이용 목록 조회" />
+      {visits.map((visit) => (
+        <UserVisitCard
+          key={visit.id}
+          id={visit.id}
+          name={visit.name}
+          male={visit.male}
+          female={visit.female}
+          date={visit.date}
+          onDelete={() => handleDelete(visit.id, visit.name)}
+        />
+      ))}
+    </Container>
   );
 };
 
 export default UserVisitList;
+
+const Container = styled.div`
+  padding-top: 12.04vh;
+`;
