8ed569e50828257c98fa32774d907d062f9d0713
hyemin <ohyemin1113@gmail.com>
2025-10-17 17:30:27 +0900

    feat : authStore 구현

---
+++ src/store/authStore.js
@@ -0,0 +1,28 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+const useAuthStore = create(
+  persist(
+    (set) => ({
+      isAuthenticated: false,
+      token: null,
+
+      setAuth: (token) =>
+        set(() => ({
+          isAuthenticated: true,
+          token: token,
+        })),
+
+      clearAuth: () =>
+        set(() => ({
+          isAuthenticated: false,
+          token: null,
+        })),
+    }),
+    {
+      name: 'auth-storage',
+    }
+  )
+);
+
+export default useAuthStore;
