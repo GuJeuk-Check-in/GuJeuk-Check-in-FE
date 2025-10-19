f069da9ae12adf6efae2b5b663620b72c198ec7c
hyemin <ohyemin1113@gmail.com>
2025-10-19 11:42:32 +0900

    fix : 중복 슬래시 제거

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import useAuthStore from '../store/authStore';
 
 export const axiosInstance = axios.create({
-  baseURL: 'https://gujeuk-api.dsmhs.kr/',
+  baseURL: 'https://gujeuk-api.dsmhs.kr',
   headers: {
     'Content-Type': 'application/json',
   },
