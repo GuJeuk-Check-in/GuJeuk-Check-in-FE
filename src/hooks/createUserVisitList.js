eb0e5dbc773c26dd6d43019fcac485379dba0031
hyemin <ohyemin1113@gmail.com>
2025-10-21 20:22:09 +0900

    fix : 현재 불필요한 인자 제거

--- src/hooks/createUserVisitList.js
+++ src/hooks/createUserVisitList.js
@@ -7,11 +7,11 @@ export const useCreateUserVisit = () => {
   return useMutation({
     mutationFn: createUserVisit,
 
-    onSuccess: (data, variables, context) => {
+    onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['visits'] });
     },
 
-    onError: (error, variables, context) => {
+    onError: (error) => {
       console.error('이용 기록 생성 중 오류 발생:', error);
       alert('등록에 실패했습니다: ' + (error.message || '알 수 없는 오류'));
     },
