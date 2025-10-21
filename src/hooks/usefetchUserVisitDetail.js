05871705c61852f111869e301d56a97145a58b63
hyemin <ohyemin1113@gmail.com>
2025-10-21 20:18:08 +0900

    feat : 시설 이용 기록 삭제 Hooks 구현

---
+++ src/hooks/usefetchUserVisitDetail.js
@@ -0,0 +1,16 @@
+import { useQuery } from '@tanstack/react-query';
+import { fetchUserVisitDetail } from '../api/visitApi';
+
+export const usefetchUserVisitDetail = (id) => {
+  const enabled = !!id && id !== 'new';
+
+  return useQuery({
+    queryKey: ['visitDetail', id],
+
+    queryFn: () => fetchUserVisitDetail(id),
+
+    enabled: enabled,
+
+    staleTime: 1000 * 60 * 5,
+  });
+};
