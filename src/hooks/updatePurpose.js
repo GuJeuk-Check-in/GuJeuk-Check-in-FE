bb3723a297c807f5dab59b3aca49444cebea6155
hyemin <ohyemin1113@gmail.com>
2025-10-21 23:19:42 +0900

    feat : 방문 목적 수정 api Hooks 구현

---
+++ src/hooks/updatePurpose.js
@@ -0,0 +1,19 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { updatePurpose } from '../api/purposeApi';
+
+export const useUpdatePurpose = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({ id, newPurpose }) => updatePurpose(id, newPurpose),
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['purposeList'] });
+      alert('방문 목적이 성공적으로 수정되었습니다.');
+    },
+
+    onError: (error) => {
+      alert(`방문 목적 수정 실패: ${error.message}`);
+    },
+  });
+};
