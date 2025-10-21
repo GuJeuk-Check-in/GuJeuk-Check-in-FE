511612abc384b725891e6724a189713388919e50
hyemin <ohyemin1113@gmail.com>
2025-10-21 20:17:08 +0900

    feat : 방문 목적 페이지 api 적용

--- src/pages/PurposeCustom.jsx
+++ src/pages/PurposeCustom.jsx
@@ -1,12 +1,44 @@
 import UseBackground from '../components/Background/UseBackground';
 import Header from '../components/Form/Header';
 import PurposeBox from '../components/Form/PurposeBox';
-import usePurposeStore from '../store/PurposeStore';
 import PurposeAddBox from '../components/Button/PurposeAddBox';
 import styled from '@emotion/styled';
+import { usePurposeList } from '../hooks/usePurposeList';
+import { useCreatePurpose } from '../hooks/createPurpose';
+import { useDeletePurposeList } from '../hooks/deletePurposeList';
 
 const PurposeCustom = () => {
-  const purposes = usePurposeStore((state) => state.purposes);
+  const { data: purposes, isLoading, isError, error } = usePurposeList();
+  const deleteMutation = useDeletePurposeList();
+
+  const handleDelete = (id) => {
+    if (window.confirm('정말로 이 방문 목적을 삭제하시겠습니까?')) {
+      deleteMutation.mutate(id);
+    }
+  };
+  if (isLoading) {
+    return (
+      <Container>
+        <UseBackground />
+        <Header title="방문 목적 커스텀" />
+        <LoadingText>방문 목적 리스트를 불러오는 중...</LoadingText>
+      </Container>
+    );
+  }
+
+  if (isError) {
+    console.error('방문 목적 리스트 로드 에러:', error);
+    return (
+      <Container>
+        <UseBackground />
+        <Header title="방문 목적 커스텀" />
+        <ErrorText>
+          데이터를 불러오는 데 실패했습니다: {error.message}
+        </ErrorText>
+      </Container>
+    );
+  }
+  console.log('가져온 목적 데이터:', purposes);
 
   return (
     <Container>
@@ -14,7 +46,12 @@ const PurposeCustom = () => {
       <Header title="방문 목적 커스텀" />
       <PurposeList>
         {purposes.map((purpose) => (
-          <PurposeBox key={purpose.id} purpose={purpose} />
+          <PurposeBox
+            key={purpose.id}
+            purpose={purpose}
+            onDelete={handleDelete}
+            isDeleting={deleteMutation.isLoading}
+          />
         ))}
         <PurposeAddBox />
       </PurposeList>
@@ -37,4 +74,14 @@ const PurposeList = styled.div`
 
 const Container = styled.div`
   padding-top: 12.04vh;
+`;
+
+const LoadingText = styled.div`
+  text-align: center;
+  font-size: 1.2rem;
+  margin-top: 50px;
+`;
+
+const ErrorText = styled(LoadingText)`
+  color: red;
 `;
