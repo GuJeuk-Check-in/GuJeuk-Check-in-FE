53db70f7530a105966913fb18d4606b19f58d468
hyemin <ohyemin1113@gmail.com>
2025-11-07 16:44:28 +0900

    feat : MainLayoutWrapper 구현

---
+++ src/components/Form/MainLayoutWrapper.jsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+const MainLayoutWrapper = ({ children }) => {
+  return <LayoutWrapper>{children}</LayoutWrapper>;
+};
+
+export default MainLayoutWrapper;
+
+const LayoutWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 86.25rem;
+  height: 80vh;
+  margin: 10vh auto;
+  box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.2);
+`;
