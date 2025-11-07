1c3ee70b670dfe98871ee40bd525cb89a3b0ba83
hyemin <ohyemin1113@gmail.com>
2025-11-07 16:44:11 +0900

    feat : MainLayout 구현

---
+++ src/components/Form/MainLayout.jsx
@@ -0,0 +1,24 @@
+import styled from '@emotion/styled';
+import LeftPage from './LeftPage';
+import RightPage from './RightPage';
+
+const MainLayout = () => {
+  return (
+    <LayoutWrapper>
+      <LeftPage />
+      <RightPage />
+    </LayoutWrapper>
+  );
+};
+
+export default MainLayout;
+
+const LayoutWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 86.25rem;
+  height: 80vh;
+  margin: 10vh auto;
+  box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.2);
+`;
