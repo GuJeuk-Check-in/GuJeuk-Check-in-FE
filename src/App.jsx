95cdbe9ca8dde45e9fb09f07414e3f48915478d9
hyemin <ohyemin1113@gmail.com>
2025-11-20 15:54:43 +0900

    fix : 새로고침 시 로그인 풀림 및 404 에러 해결 (앱 초기화 로직 추가)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,51 @@
 import { Global, css } from '@emotion/react';
 import Router from './Router';
+import axios from 'axios';
+import { useEffect } from 'react';
+import useAuthStore from './store/authStore';
 
 const App = () => {
+  const { setAuth, logout, isInitializing, finishInitialization } =
+    useAuthStore();
+
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      try {
+        const response = await axios.patch(
+          `${import.meta.env.VITE_API_BASE_URL}/admin/re-issue`,
+          {},
+          { withCredentials: true }
+        );
+
+        const { token } = response.data;
+
+        setAuth(token);
+      } catch (error) {
+        console.log('비로그인 상태 또는 세션 만료');
+        logout();
+        finishInitialization();
+      }
+    };
+
+    checkLoginStatus();
+  }, [setAuth, logout, finishInitialization]);
+
+  if (isInitializing) {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+          fontSize: '1.5rem',
+        }}
+      >
+        로그인 확인 중...
+      </div>
+    );
+  }
+
   return (
     <>
       <Global
