8e945a452ee60cf0cc3a4756bba736dc7406b0c1
hyemin <ohyemin1113@gmail.com>
2025-12-07 23:30:14 +0900

    feat :: theme 파일 추가

---
+++ src/styles/theme.js
@@ -0,0 +1,67 @@
+const theme = {
+  colors: {
+    primary: {
+      main: '#3d72b3',
+      dark: '#0f50a0',
+      light: '#6f95c4',
+      pale: '#c2d5ec',
+      bg: '#f9fbff',
+      text: '#2f68c2',
+    },
+
+    accent: {
+      red: '#dc3545',
+      pink: '#f86879',
+      active: '#007bff',
+      hoverRed: '#c00',
+    },
+
+    gray: {
+      black: '#000000',
+      dark: '#2e2e32',
+      text: '#333333',
+      sub: '#565656',
+      desc: '#969698',
+      border: '#404040',
+      lightBorder: '#aaa',
+      disabled: '#a0a0a0',
+    },
+
+    bg: {
+      white: '#ffffff',
+      light: '#f0f0f0',
+      disabled: '#f8f8f8',
+    },
+  },
+
+  fontSize: {
+    xs: '0.95rem',
+    sm: '1.1rem',
+    base: '1.25rem',
+    lg: '1.5rem',
+    xl: '2rem',
+    xxl: '2.5rem',
+    logo: '4rem',
+  },
+
+  borderRadius: {
+    sm: '0.5rem',
+    md: '1.25rem',
+    lg: '2.2rem',
+    round: '3.125rem',
+  },
+
+  shadow: {
+    sm: '0 0.125rem 0.375rem rgba(0, 0, 0, 0.08)',
+    base: '0 0.25rem 0.625rem rgba(0, 0, 0, 0.1)',
+    hover: '0 0.25rem 0.9375rem rgba(0, 0, 0, 0.15)',
+    card: '0 0.5rem 1rem rgba(0, 0, 0, 0.2)',
+  },
+
+  device: {
+    mobile: 'screen and (max-width: 768px)',
+    tablet: 'screen and (max-width: 1200px)',
+  },
+};
+
+export default theme;
