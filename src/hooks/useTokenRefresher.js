31fa4cefaf03b993b935eb84a03757095a0ad9ac
hyemin <ohyemin1113@gmail.com>
2025-12-12 08:27:04 +0900

    feat :: 토큰 자동 갱신을 위한 useTokenRefresher 훅 추가

---
+++ src/hooks/useTokenRefresher.js
@@ -0,0 +1,34 @@
+import { useEffect } from 'react';
+import useAuthStore from '../store/authStore';
+import axiosInstance from '../api/axiosInstance';
+
+const useTokenRefresher = () => {
+  const { accessToken, setAuth, logout } = useAuthStore();
+
+  useEffect(() => {
+    if (!accessToken) return;
+
+    const refreshCycle = 1000 * 50;
+
+    const timer = setInterval(async () => {
+      console.log('50초 경과');
+      try {
+        const res = await axiosInstance.patch('admin/re-issue');
+
+        const newAccess = res.data?.accessToken;
+        const newRefresh = res.data?.refreshToken;
+
+        if (newAccess && newRefresh) {
+          console.log('토큰 갱신 성공');
+          setAuth(newAccess, newRefresh);
+        }
+      } catch (error) {
+        console.error('자동 갱신 실패:', error);
+      }
+    }, refreshCycle);
+
+    return () => clearInterval(timer);
+  }, [accessToken, setAuth]);
+};
+
+export default useTokenRefresher;
