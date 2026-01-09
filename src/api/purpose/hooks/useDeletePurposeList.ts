b2b0f67a9903d29fbda91a7d19539435d10e6286
hyemin <ohyemin1113@gmail.com>
2026-01-09 19:36:51 +0900

    fix :: 삭제 시 모달 사용으로 인힌 onError 제거

--- src/api/purpose/hooks/useDeletePurposeList.ts
+++ src/api/purpose/hooks/useDeletePurposeList.ts
@@ -7,19 +7,8 @@ export const useDeletePurposeList = () => {
 
   return useMutation<void, AxiosError<{ message?: string }>, number>({
     mutationFn: deletePurpose,
-
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['purposeList'] });
-      alert('방문 목적이 성공적으로 삭제되었습니다.');
-    },
-
-    onError: (error) => {
-      const message =
-        error.response?.data?.message ??
-        error.message ??
-        '삭제 중 오류가 발생했습니다.';
-
-      alert(message);
     },
   });
 };
