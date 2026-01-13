9b434e71a8f3484a876a5e084d59295967713d39
hyemin <ohyemin1113@gmail.com>
2026-01-13 17:16:09 +0900

    fix :: onError 분리

---
+++ src/features/purpose/create-purpose/model/useCreatePurpose.ts
@@ -0,0 +1,23 @@
+import { createPurpose } from '../../../../api/purpose/api';
+import { AxiosError } from 'axios';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import {
+  CreatePurposeRequest,
+  PurposeResponse,
+} from '@entities/purpose/model/types';
+
+export const useCreatePurpose = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation<
+    PurposeResponse,
+    AxiosError<{ message?: string }>,
+    CreatePurposeRequest
+  >({
+    mutationFn: createPurpose,
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['purposeList'] });
+    },
+  });
+};
