f57f64f29f2682b6329b4cc3bcf67eb228498a8a
hyemin <ohyemin1113@gmail.com>
2026-02-15 22:11:00 +0900

    feat :: 거주기 저장할 store 구축

---
+++ src/entities/residence/model/residenceStore.ts
@@ -0,0 +1,13 @@
+import { create } from 'zustand';
+import { ResidenceStore } from './types';
+
+export const useResidenceStore = create<ResidenceStore>((set) => ({
+  residences: [],
+
+  updateResidence: (id, newResidenceName) =>
+    set((state) => ({
+      residences: state.residences.map((r) =>
+        r.id === id ? { ...r, residenceName: newResidenceName } : r
+      ),
+    })),
+}));
