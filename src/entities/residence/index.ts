d5c4a5f44932cf9960250fee7c5b56f4ff8926f8
hyemin <ohyemin1113@gmail.com>
2026-02-17 20:34:18 +0900

    feat :: 절대 경로 추가

--- src/entities/residence/index.ts
+++ src/entities/residence/index.ts
@@ -2,3 +2,4 @@ export * from './api/residence.api';
 export * from './model/residenceStore';
 export * from './model/types';
 export * from './model/useResidenceList';
+export * from './ui/ResidenceCard';
