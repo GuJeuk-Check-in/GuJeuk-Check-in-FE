0245c9fe77f466130ba802c52776f73f0640445f
hyemin <ohyemin1113@gmail.com>
2026-02-17 23:03:34 +0900

    feat :: 거주지 보드 컴포넌트 추가

---
+++ src/widgets/residence/ui/ResidenceBoard.tsx
@@ -0,0 +1,124 @@
+import styled from '@emotion/styled';
+import { DndContext, closestCenter } from '@dnd-kit/core';
+import { SortableContext, rectSortingStrategy } from '@dnd-kit/sortable';
+import { useResidenceList } from '@entities/residence/index';
+import { ResidenceCard } from '@entities/residence/index';
+import { CreateResidenceModal } from '@features/residence/create-residence';
+import {
+  useUpdateResidence,
+  useDeleteResidence,
+  useReorderResidence,
+} from '@features/residence/index';
+import { SortablePurposeItem } from '@shared/ui/Form/SortablePurposeItem';
+
+export const ResidenceBoard = () => {
+  const { data: residences, isLoading, isError, error } = useResidenceList();
+  const { items, sensors, handleDragEnd } = useReorderResidence(
+    residences || []
+  );
+
+  const { mutate: updateMutate, isPending: isUpdating } = useUpdateResidence();
+  const {
+    mutate: deleteMutate,
+    isPending: isDeleting,
+    variables: deletingVariable,
+  } = useDeleteResidence();
+
+  const handleUpdate = (id: number, newResidence: string) => {
+    updateMutate({ id, residence: newResidence });
+  };
+
+  const handleDelete = (id: number) => {
+    deleteMutate(id);
+  };
+
+  if (isLoading) {
+    return (
+      <LoadingOverlay>
+        <LoadingBox>
+          <p>데이터를 불러오는 중</p>
+          <p>잠시만 기다려주세요...</p>
+        </LoadingBox>
+      </LoadingOverlay>
+    );
+  }
+
+  if (isError) {
+    return (
+      <ErrorText>
+        데이터 로드 실패: {error instanceof Error ? error.message : '오류 발생'}
+      </ErrorText>
+    );
+  }
+
+  return (
+    <DndContext
+      sensors={sensors}
+      collisionDetection={closestCenter}
+      onDragEnd={handleDragEnd}
+    >
+      <ResidenceListGrid>
+        <SortableContext items={items} strategy={rectSortingStrategy}>
+          {items.map((residence) => (
+            <SortablePurposeItem key={residence.id} id={residence.id}>
+              <ResidenceCard
+                residence={residence}
+                onDelete={handleDelete}
+                onUpdate={({
+                  id,
+                  newResidence,
+                }: {
+                  id: number;
+                  newResidence: string;
+                }) => handleUpdate(id, newResidence)}
+                isDeleting={
+                  (isDeleting && deletingVariable === residence.id) ||
+                  isUpdating
+                }
+              />
+            </SortablePurposeItem>
+          ))}
+        </SortableContext>
+        <CreateResidenceModal />
+      </ResidenceListGrid>
+    </DndContext>
+  );
+};
+
+const ResidenceListGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 1.5rem;
+  justify-items: center;
+  width: 100%;
+  max-width: 75rem;
+  margin: 0 auto;
+  padding: 2.5rem 0;
+`;
+
+const ErrorText = styled.div`
+  text-align: center;
+  color: red;
+  margin-top: 2rem;
+`;
+
+const LoadingOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.7);
+  z-index: 9999;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LoadingBox = styled.div`
+  background: rgba(255, 255, 255, 0.3);
+  padding: 30px 50px;
+  border-radius: 10px;
+  color: #fff;
+`;
