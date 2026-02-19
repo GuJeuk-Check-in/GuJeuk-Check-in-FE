5ea819ce2654d252493df847a52ab9d82ebd5ebf
hyemin <ohyemin1113@gmail.com>
2026-02-19 12:07:44 +0900

    feat :: setResidences 추가 Entire-Checkpoint: 8624a4ed3d68

--- src/entities/residence/model/residenceStore.ts
+++ src/entities/residence/model/residenceStore.ts
@@ -4,6 +4,8 @@ import { ResidenceStore } from './types';
 export const useResidenceStore = create<ResidenceStore>((set) => ({
   residences: [],
 
+  setResidences: (residences) => set({ residences }),
+
   updateResidence: (id, newResidenceName) =>
     set((state) => ({
       residences: state.residences.map((r) =>
