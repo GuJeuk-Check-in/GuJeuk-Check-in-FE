615c498758ae074c4f8596f56430e0af5bc0b78e
hyemin <ohyemin1113@gmail.com>
2026-02-23 18:52:58 +0900

    feat :: useTokenRefresher 파일 이동 Entire-Checkpoint: 669f14380f15

---
+++ src/entities/auth/model/useTokenRefresher.ts
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+import { useAuthStore } from '@entities/auth/index';
+import axiosInstance from '@shared/api/axiosInstance';
+
+export const useTokenRefresher = () => {
+  const { accessToken, setAuth, logout } = useAuthStore();
+  useEffect(() => {
+    if (!accessToken) return;
+    const refreshCycle = 1000 * 3300;
+    const timer = setInterval(async () => {
+      try {
+        const res = await axiosInstance.patch('admin/re-issue');
+        const newAccess = res.data?.accessToken;
+        const newRefresh = res.data?.refreshToken;
+        if (newAccess && newRefresh) {
+          setAuth(newAccess, newRefresh);
+        }
+      } catch (error) {
+        console.error('토큰 갱신 실패:', error);
+        logout();
+      }
+    }, refreshCycle);
+    return () => clearInterval(timer);
+  }, [accessToken, setAuth, logout]);
+};
