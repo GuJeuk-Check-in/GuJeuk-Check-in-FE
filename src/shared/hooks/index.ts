3a8e9327c15398331d3fcd3ed9c948579381d325
hyemin <ohyemin1113@gmail.com>
2026-03-25 14:38:33 +0900

    chore :: 파일 위치 변경으로 인한 절대 경로 삭제 Entire-Checkpoint: 36dc1de7dbf6

--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -2,4 +2,3 @@ export { useCheck } from './useCheck';
 export { useCounter } from './useCounter';
 export { useInput } from './useInput';
 export { useModal } from './useModal';
-export { useUserDetailPage } from './useUserDetailPage';
