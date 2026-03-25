dd8038ddaf568650e929eee5e35f44ac839c52c7
hyemin <ohyemin1113@gmail.com>
2026-03-25 14:51:29 +0900

    feat :: 페이지 공용 레이아웃 Entire-Checkpoint: 3872d581c93e

---
+++ src/widgets/GlobalLayout/ui/PageLayout.tsx
@@ -0,0 +1,39 @@
+import styled from '@emotion/styled';
+import { ReactNode } from 'react';
+import { UseBackground } from '@shared/ui/Background';
+import { Header } from '@widgets/GlobalLayout';
+
+interface PageLayoutProps {
+  children: ReactNode;
+  showHeader?: boolean;
+}
+
+export const PageLayout = ({ children, showHeader = true }: PageLayoutProps) => {
+  return (
+    <Container>
+      <UseBackground />
+      {showHeader && <Header />}
+      <ContentWrapper>{children}</ContentWrapper>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  flex: 1;
+  box-sizing: border-box;
+  display: flex;
+  max-height: 100vh;
+  overflow-y: hidden;
+`;
+
+const ContentWrapper = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  padding: 3.5rem 3.75rem;
+  gap: 1.25rem;
+  box-sizing: border-box;
+  overflow-y: scroll;
+`;
