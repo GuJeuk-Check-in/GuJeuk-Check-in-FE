ee409c6db813a0a515c98b79c91f7082be982f58
hyemin <ohyemin1113@gmail.com>
2026-03-27 19:46:26 +0900

    chore :: 불필요한 타입 제거 Entire-Checkpoint: 35372e407d4d

--- src/features/user/user-update/model/useUpdateUser.ts
+++ src/features/user/user-update/model/useUpdateUser.ts
@@ -2,11 +2,6 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { updateUserInformation } from '@entities/user/api/user.api';
 import { UserInformation } from '@entities/user';
 
-type UpdateUserPayload = {
-  id: number;
-  data: Omit<UserInformation, 'id'>;
-};
-
 export const useUpdateUserInformation = () => {
   const queryClient = useQueryClient();
 
