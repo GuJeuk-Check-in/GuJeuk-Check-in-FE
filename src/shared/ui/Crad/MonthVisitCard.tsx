c047a39fa52a246acd80244c35e52a71ca43e803
abc100 <abccba111307@gmail.com>
2026-03-28 11:47:42 +0900

    refactor :: 불필요한 import 제거

--- src/shared/ui/Crad/MonthVisitCard.tsx
+++ src/shared/ui/Crad/MonthVisitCard.tsx
@@ -1,5 +1,4 @@
 import styled from '@emotion/styled';
-import { MonthVisitButton } from '../Button/MonthVisitButton';
 
 interface MonthVisitCardProps {
   month: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
