a5aa27b96f830fd63ce1648a2d63445b68da7f8a
hyemin <ohyemin1113@gmail.com>
2026-06-16 20:00:53 +0900

    feat :: 공개 axiosInstance 추가 Entire-Checkpoint: 35372e407d4d

---
+++ src/shared/api/publicAxiosInstance.ts
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+export const publicAxiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: { 'Content-Type': 'application/json' },
+});
