f860184a523bb17ae76a04cd1942d66e1aad0895
hyemin <ohyemin1113@gmail.com>
2026-08-04 21:49:44 +0900

    hotfix :: 옵션 목록 조회 ready 상태로 제한 Entire-Checkpoint: 35372e407d4d

--- src/entities/purpose/model/usePurposeList.ts
+++ src/entities/purpose/model/usePurposeList.ts
@@ -2,11 +2,16 @@ import { useQuery } from '@tanstack/react-query';
 import { purposeList } from '../api/purpose.api';
 import { PurposeResponse } from './types';
 import { AxiosError } from 'axios';
+import { useIsRemoteSyncReady } from '@shared/lib';
 
 export const usePurposeList = () => {
+  const isRemoteSyncReady = useIsRemoteSyncReady();
+
   return useQuery<PurposeResponse[], AxiosError<{ message?: string }>>({
     queryKey: ['purposeList'],
     queryFn: purposeList,
+    enabled: isRemoteSyncReady,
+    retry: false,
     staleTime: 1000 * 60 * 5,
   });
 };
